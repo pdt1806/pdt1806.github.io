853595522fbefbaf43d952d2512745e0b3beb2ad
pdteggman <78996937+pdt1806@users.noreply.github.com>
2023-01-06 18:20:44 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var images = ['wallpaper.jpg', 'wallpaper2.jpg'];
-document.getElementsByClassName('mainview')[0].style.backgroundImage = 'url(''img/' + images[Math.floor(Math.random() * images.length)] + ')';
+document.getElementsByClassName('mainview')[0].style.backgroundImage = 'url('img/ + images[Math.floor(Math.random() * images.length)] + ')';
 
 function playSound() {
 	var audio = document.getElementById("audio");
@@ -18,47 +18,47 @@ function playSound3() {
 }
 
 function reverseString(str) {
-		var newString = '';
-		for (var i = str.length - 1; i >= 0; i--) {
-			newString += str[i];
-		}
-		return newString;
+	var newString = '';
+	for (var i = str.length - 1; i >= 0; i--) {
+		newString += str[i];
 	}
-	function encodeFunc() {
-		var input = document.getElementById("inputbox").value;
-		var output = '';
-		for (var i = 0; i < input.length; i++) {
-			output = output + String.fromCharCode(input.charCodeAt(i)+1)
-		}
-		output = reverseString(output)
-		document.getElementById("outputbox").value = output;
+	return newString;
+}
+function encodeFunc() {
+	var input = document.getElementById("inputbox").value;
+	var output = '';
+	for (var i = 0; i < input.length; i++) {
+		output = output + String.fromCharCode(input.charCodeAt(i)+1)
 	}
-	function decodeFunc() {
-		var input = document.getElementById("inputbox").value;
-		var output = '';
-		for (var i = 0; i < input.length; i++) {
-			output = output + String.fromCharCode(input.charCodeAt(i)-1)
-		}
-		output = reverseString(output)
-		document.getElementById("outputbox").value = output;
+	output = reverseString(output)
+	document.getElementById("outputbox").value = output;
+}
+function decodeFunc() {
+	var input = document.getElementById("inputbox").value;
+	var output = '';
+	for (var i = 0; i < input.length; i++) {
+		output = output + String.fromCharCode(input.charCodeAt(i)-1)
 	}
-	function cls() {
-		document.getElementById("inputbox").value = '';
+	output = reverseString(output)
+	document.getElementById("outputbox").value = output;
+}
+function cls() {
+	document.getElementById("inputbox").value = '';
+}
+function copy() {
+	if (document.getElementById("outputbox").value != '') {
+	  var output = document.getElementById("outputbox");
+		output.select();
+		output.setSelectionRange(0, 99999);
+		document.execCommand("copy");
+		document.getElementById("copyAlert").innerHTML = '    Copied to clipboard!';
+		setTimeout(function(){
+		document.getElementById("copyAlert").innerHTML = '';
+		}, 3000);
+	} else {
+		document.getElementById("copyAlert").innerHTML = '    Nothing to copy.';
+		setTimeout(function(){
+		document.getElementById("copyAlert").innerHTML = '';
+		}, 3000);
 	}
-	function copy() {
-		if (document.getElementById("outputbox").value != '') {
-		  var output = document.getElementById("outputbox");
-			output.select();
-			output.setSelectionRange(0, 99999);
-			document.execCommand("copy");
-			document.getElementById("copyAlert").innerHTML = '    Copied to clipboard!';
-			setTimeout(function(){
-			document.getElementById("copyAlert").innerHTML = '';
-			}, 3000);
-		} else {
-			document.getElementById("copyAlert").innerHTML = '    Nothing to copy.';
-			setTimeout(function(){
-			document.getElementById("copyAlert").innerHTML = '';
-			}, 3000);
-		}
-	}
+}

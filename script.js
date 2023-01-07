be5997ce5124bbbadb2a567deb66e63b04cf5718
pdteggman <78996937+pdt1806@users.noreply.github.com>
2023-01-06 22:25:12 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var images = ['wallpaper.jpg', 'wallpaper2.jpg'];
-document.write("<style type='text/css'>body {background-image:url(img/"+ images[Math.floor(Math.random() * images.length)] +");}</style>");
+document.write("<style type='text/css'>body {background-image:linear-gradient(rgba(0,0,0,0.1), rgba(0,0,0,0.1)), url(img/"+ images[Math.floor(Math.random() * images.length)] +");}</style>");
 
 function playSound() {
 	var audio = document.getElementById("audio");

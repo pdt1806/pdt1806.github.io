3aba2d3729d4eb3bf13c90260dd1f901152e2fb3
pdteggman <78996937+pdt1806@users.noreply.github.com>
2023-01-07 22:25:21 -0800

    Create script_phatsua.js

---
+++ script_phatsua.js
@@ -0,0 +1,15 @@
+function playSound() {
+	var audio = document.getElementById("audio");
+	audio.currentTime = 0;
+	audio.play();
+}
+function playSound2() {
+	var audio = document.getElementById("audio2");
+	audio.currentTime = 0;
+	audio.play();
+}
+function playSound3() {
+	var audio = document.getElementById("audio3");
+	audio.currentTime = 0;
+	audio.play();
+}

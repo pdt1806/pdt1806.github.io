5b3ef5f4012ea4b355e3ba71275960766bbfca8e
pdteggman <78996937+pdt1806@users.noreply.github.com>
2023-01-09 18:10:18 -0800

    wallpapers

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-var images = ['wallpaper.jpg', 'wallpaper2.jpg'];
+var images = ['wallpaper.jpg', 'wallpaper2.jpg', 'wallpaper3.jpg', 'wallpaper4.jpg', 'wallpaper5.png', 'wallpaper6.png', 'wallpaper7.jpg', 'wallpaper8.png', 'wallpaper9.jpg', 'wallpaper10.jpg'];
 document.write("<style type='text/css'>body {background-image:linear-gradient(rgba(0,0,0,0.1), rgba(0,0,0,0.1)), url(img/"+ images[Math.floor(Math.random() * images.length)] +");}</style>");

246ae160696c9ccad0d0f7b87090598b1982a765
pdt1806 <nmphuc1806@gmail.com>
2023-11-08 18:15:48 -0800

    refactor: :art: switch place

--- src/pages/components/Box/Connections.tsx
+++ src/pages/components/Box/Connections.tsx
@@ -10,15 +10,6 @@ const Connections = () => {
           title="GitHub"
         />
       </a>
-      <a href="https://facebook.com/pdteggman" target="_blank">
-        <img
-          style={{ filter: "invert(1)" }}
-          src="/icon/social-media/facebook.svg"
-          className="connections-icon"
-          alt="Facebook"
-          title="Facebook"
-        />
-      </a>
       <a href="https://instagram.com/benny_ng08" target="_blank">
         <img
           style={{ filter: "invert(1)" }}
@@ -26,6 +17,15 @@ const Connections = () => {
           className="connections-icon"
           alt="Instagram"
           title="Instagram"
+        />
+      </a>
+      <a href="https://facebook.com/pdteggman" target="_blank">
+        <img
+          style={{ filter: "invert(1)" }}
+          src="/icon/social-media/facebook.svg"
+          className="connections-icon"
+          alt="Facebook"
+          title="Facebook"
         />
       </a>
       <a href="https://x.com/pdteggman" target="_blank">
